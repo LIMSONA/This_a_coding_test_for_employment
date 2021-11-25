031eaa2fc3164ce5f3e38ff6cbe6f81f9d6cedea
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-11-25 15:15:32 +0900

    Delete part1. lambda.py

--- part1. lambda.py
+++
@@ -1,8 +0,0 @@
-array=[('a',4),('b',2),('c',3)]
-
-def sona(x):
-    return x[1]
-
-
-print(sorted(array, key=sona))  # >>>> [('b', 2), ('c', 3), ('a', 4)]
-print(sorted(array, key=lambda x: x[1]))    # >>>> [('b', 2), ('c', 3), ('a', 4)]
